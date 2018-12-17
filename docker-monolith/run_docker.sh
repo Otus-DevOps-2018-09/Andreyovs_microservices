388e1849f1b93520c89a1c714a1c70c2144d1750
Andrey <andreyovs@yandex.ru>
2018-12-17 13:52:05 +0300

    updatetd alpine version

--- docker-monolith/run_docker.sh
+++ docker-monolith/run_docker.sh
@@ -4,26 +4,26 @@ docker network create reddit
 
 echo 'step 1'
 docker run -d --network=reddit \
---network-alias=post_db_srv \
---network-alias=comment_db_srv -v reddit_db:/data/db mongo:latest
+--network-alias=post_db \
+--network-alias=comment_db -v reddit_db:/data/db mongo:latest
 
 echo 'step 2'
 docker run -d --network=reddit \
 --network-alias=post_srv \
--e POST_DATABASE_HOST='post_db_srv' \
--e POST_DATABASE='posts_srv' \
+-e POST_DATABASE_HOST='post_db' \
+-e POST_DATABASE='posts' \
 andreyovs/post:latest
 
 echo 'step 3'
 docker run -d --network=reddit \
 --network-alias=comment_srv \
--e COMMENT_DATABASE_HOST='comment_db_srv' \
--e COMMENT_DATABASE='comments_srv' \
+-e COMMENT_DATABASE_HOST='comment_db' \
+-e COMMENT_DATABASE='comments' \
 andreyovs/comment:latest
 
 echo 'step 4'
 docker run -d --network=reddit \
--p 9292:9292\
--e POST_SERVICE_HOST='post_srv' \
--e COMMENT_SERVICE_HOST='comment_srv' \
+-p 9292:9292 \
+-e POST_SERVICE_HOST='post' \
+-e COMMENT_SERVICE_HOST='comment' \
 andreyovs/ui:latest

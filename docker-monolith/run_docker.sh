05d8dfb7e773e28b9594ba6ee017b562376a2408
Andrey <andreyovs@yandex.ru>
2018-12-17 10:53:33 +0300

    test

--- docker-monolith/run_docker.sh
+++ docker-monolith/run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker kill $(docker ps -q)
+docker network create reddit
 
 echo 'step 1'
 docker run -d --network=reddit \
@@ -11,18 +12,18 @@ docker run -d --network=reddit \
 --network-alias=post_srv \
 -e POST_DATABASE_HOST='post_db_srv' \
 -e POST_DATABASE='posts_srv' \
-andreyovs/post:1.0
+andreyovs/post:latest
 
 echo 'step 3'
 docker run -d --network=reddit \
 --network-alias=comment_srv \
 -e COMMENT_DATABASE_HOST='comment_db_srv' \
 -e COMMENT_DATABASE='comments_srv' \
-andreyovs/comment:1.0
+andreyovs/comment:latest
 
 echo 'step 4'
 docker run -d --network=reddit \
--p 9292:9292 \
+-p 9292:9292\
 -e POST_SERVICE_HOST='post_srv' \
 -e COMMENT_SERVICE_HOST='comment_srv' \
-andreyovs/ui:1.0
+andreyovs/ui:latest

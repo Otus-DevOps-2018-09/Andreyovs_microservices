27fdfa5f3e1d786d5f27327e329959b5ef454b26
Andrey <andreyovs@yandex.ru>
2019-01-06 19:43:08 +0300

    HW 21 compleeted

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,13 +1,31 @@
-#/bin/bash
-export USER_NAME=andreyovs
-export PROMETHEUS_VER=latest
-export UI_VER=latest
-export COMMENT_VER=latest
-export MONGODB_EXPORTER_VER=latest
-export MONGO_VER=latest
-export BLACKBOX_EXPORTER_VER=latest
-export UI_HOSTPORT=9292
-export UI_CONTAINERPORT=
-export POST_VER=
-export NODE_EXPORTER_VER =latest
-docker-compose up -d
+#!/bin/bash
+eval $(docker-machine env docker-host)
+docker kill $(docker ps -q)
+docker network create reddit
+docker volume create reddit_db
+
+echo 'step 1'
+docker run -d --network=reddit \
+--network-alias=post_db_srv \
+--network-alias=comment_db_srv -v reddit_db:/data/db mongo:latest
+
+echo 'step 2'
+docker run -d --network=reddit \
+--network-alias=post_srv \
+-e POST_DATABASE_HOST='post_db_srv' \
+-e POST_DATABASE='posts_srv' \
+andreyovs/post:latest
+
+echo 'step 3'
+docker run -d --network=reddit \
+--network-alias=comment_srv \
+-e COMMENT_DATABASE_HOST='comment_db_srv' \
+-e COMMENT_DATABASE='comments_srv' \
+andreyovs/comment:latest
+
+echo 'step 4'
+docker run -d --network=reddit \
+-p 9292:9292 \
+-e POST_SERVICE_HOST='post_srv' \
+-e COMMENT_SERVICE_HOST='comment_srv' \
+andreyovs/ui:latest
